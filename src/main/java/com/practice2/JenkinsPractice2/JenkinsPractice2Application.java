aadc33093d01b0e2e92cb8c1ec8580d180dc68d0
Atul Kumbhar <atul.kumbhar1985@gmail.com>
2021-06-15 23:43:37 +0530

    First Commit

---
+++ src/main/java/com/practice2/JenkinsPractice2/JenkinsPractice2Application.java
@@ -0,0 +1,13 @@
+package com.practice2.JenkinsPractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPractice2Application.class, args);
+	}
+
+}
