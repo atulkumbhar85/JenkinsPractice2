0af631d2e8b1dd6307708892b6df006b7218b124
Atul Kumbhar <atul.kumbhar1985@gmail.com>
2021-06-15 23:57:03 +0530

    Modified java app

--- src/main/java/com/practice2/JenkinsPractice2/JenkinsPractice2Application.java
+++ src/main/java/com/practice2/JenkinsPractice2/JenkinsPractice2Application.java
@@ -2,12 +2,19 @@ package com.practice2.JenkinsPractice2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsPractice2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPractice2Application.class, args);
 	}
-
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("<h1>Hello %s!</h1> <h2>This is sample header</h2>", name);
+	}
 }
